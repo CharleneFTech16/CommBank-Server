a9137fc1960a98b72d6f18ab00723f707b92c203
CharleneFTech16 <charlene0312009@gmail.com>
2026-06-19 10:36:28 +1000

    Add Icon field to Goal model

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -27,4 +27,7 @@ public class Goal
 
     [BsonRepresentation(BsonType.ObjectId)]
     public string? UserId { get; set; }
+
+    [BsonRepresentation(BsonType.String)]
+    public string? Icon { get; set; }
 }
